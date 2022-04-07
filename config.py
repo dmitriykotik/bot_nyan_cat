bd89b337bdce3a73b7430ab1a2a83067edb7dbbc
dmitriykotik <68951929+dmitriykotik@users.noreply.github.com>
2022-04-08 00:27:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'OTQwOTQ0OTUwNzE0MDU2NzE1.YgOxRg.jDtCHt8UvldsGtj0CH1QIjDZgvQ',
+    'token': '(токен скрыт)',
     'bot': 'Nyan cat',
-    'id': 940944950714056715,
+    'id': (Client ID),
     'prefix': '!'
 }
